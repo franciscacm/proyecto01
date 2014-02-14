517d083ab7e035389a83d1344eca6e4ec8596aa6
francisca <fcolorad@hotmail.com>
2014-02-14 11:27:19 -0600

    proyecto 2  para moviles

---
+++ js/acciones.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+  document.addEventListener("deviceready",function(){
+	  $('#B1').tap (function() {
+	   navigator.notification.beep(1);
+	  
+	  }); // tap de B1
+  });// device ready
+}) //ready
