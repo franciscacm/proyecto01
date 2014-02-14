f17956159ef70b31bea3771a50684ab1288570fd
francisca <fcolorad@hotmail.com>
2014-02-14 12:40:26 -0600

    boton vibrar

--- js/acciones.js
+++ js/acciones.js
@@ -3,7 +3,9 @@ $(document).ready(function(){
   document.addEventListener("deviceready",function(){
 	  $('#B1').tap (function() {
 	   navigator.notification.beep(1);
-	  
+	  $('#V1').tap (function() {
+		  navigator.notification.vibrate(500); 
+	  });// tap de V1
 	  }); // tap de B1
   });// device ready
 }); //ready
